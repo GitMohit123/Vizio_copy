3452497c5e92cea8c924fb857c3f4dba8a7ae01c
shashwat-j <shashwatjain_ec21a18_23@dtu.ac.in>
2024-06-25 22:36:26 +0530

    create project

---
+++ VidzSpace/src/components/PopUp/UploadingProgress.jsx
@@ -0,0 +1,56 @@
+import React, { useContext } from 'react'
+import HomeContext from '../../context/homePage/HomeContext';
+
+const UploadingProgress = () => {
+
+    const {
+        setSelectedFiles,
+        setSelectedFolders,
+        selectedFiles,
+        selectedFolders,
+        isUploadingFiles,
+        selectedFilesWithUrls,
+      } = useContext(HomeContext);
+
+  return (
+    <div className="absolute w-1/5 max-h-[167.2px] flex justify-end items-end z-20 right-5 bottom-5">
+      <div className="popup bg-[#383838] h-full max-h-[167.2px] overflow-auto no-scrollbar w-full p-5 flex flex-col rounded-xl border-2 border-[#4c4c4c] text-white">
+        <h3 className='text-lg text-center w-full'>Uploading files</h3>
+        {selectedFiles?.map((file, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-row gap-3 items-center justify-between py-1"
+                  >
+                    <p className="text-sm truncate max-w-[75%]">{file.path}</p>
+                    {
+                    //  isUploadingFiles?
+                    selectedFilesWithUrls.length>=index && selectedFilesWithUrls[index]?.isUploading===true?
+                     <div className="w-6 h-6 rounded-full animate-spin border border-solid border-yellow-500 border-t-transparent shadow-md"></div>
+                    :
+                     <p onClick={()=>{setSelectedFiles((currFiles)=>currFiles.filter((currFile)=>currFile.path !== file.path || currFile.size !== file.size))}} className="cursor-pointer">
+                      x</p>
+                    }
+                  </div>
+        ))}
+        {selectedFolders?.map((folder, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-row gap-3 items-center justify-between py-1"
+                  >
+                    <p className="text-sm truncate max-w-[75%]">{folder.name}</p>
+                    {
+                    //  isUploadingFiles?
+                    selectedFilesWithUrls.length>=index + selectedFiles.length && selectedFilesWithUrls[index + selectedFiles.length]?.isUploading===true?
+                     <div className="w-6 h-6 rounded-full animate-spin border border-solid border-yellow-500 border-t-transparent shadow-md"></div>
+                    :
+                     <p onClick={()=>{setSelectedFiles((currFiles)=>currFiles.filter((currFile)=>currFile.path !== file.path || currFile.size !== file.size))}} className="cursor-pointer">
+                      x</p>
+                    }
+                  </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default UploadingProgress
